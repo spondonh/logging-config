6bbfe85c8d732bf5fb9beb8a5c6f0af0dd756aa1
Spondizzle <spondonh@gmail.com>
2024-05-26 11:32:56 -0400

    Renamed file to account for an available name on PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='logging_config',
+    name='logging_configuration',
     version='0.1.0',
     packages=find_packages(),
     description='A simple logging configuration module',
